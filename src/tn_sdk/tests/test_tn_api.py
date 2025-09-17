a7170033325288de6bef37155533039e4b3dd0fc
abdallahkaram1994 <abdallah.karam@tripninja.io>
2025-09-17 14:48:48 -0300

    Update test_tn_api.py

--- src/tn_sdk/tests/test_tn_api.py
+++ src/tn_sdk/tests/test_tn_api.py
@@ -1,6 +1,7 @@
 import unittest
 
 from tn_sdk import TnApi
+from tn_sdk.utils.constants import SANDBOX_API_URL, PRODUCTION_API_URL
 
 
 class TestTnApi(unittest.TestCase):
@@ -12,9 +13,9 @@ class TestTnApi(unittest.TestCase):
     def test_tn_api__instantiate_sandbox__returns_api_details(self):
         self.assertEqual(self.api_sandbox._access_token, "abc123")
         self.assertEqual(self.api_sandbox._refresh_token, "refresh456")
-        self.assertEqual(self.api_sandbox.base_url, "https://sandbox.tripninja.io")
+        self.assertEqual(self.api_sandbox.base_url, SANDBOX_API_URL)
 
     def test_tn_api__instantiate_prod__returns_api_details(self):
         self.assertEqual(self.api_prod._access_token, "abc123")
         self.assertEqual(self.api_prod._refresh_token, "refresh456")
-        self.assertEqual(self.api_prod.base_url, "https://api.tripninja.io")
+        self.assertEqual(self.api_prod.base_url, PRODUCTION_API_URL)
