4a1814d05c1c11c124db45d22473221d7376e766
flokananta-TripNinja <felix.lokananta@tripninja.io>
2025-09-17 10:45:17 -0600

    - update dummy test

--- src/tn_sdk/tests/test_dummy_for_sonar.py
+++ src/tn_sdk/tests/test_dummy_for_sonar.py
@@ -1,5 +1,2 @@
-import unittest
-
-class DummyTestForSonar(unittest.TestCase):
-    def test_dummy_test__different_input__returns_not_equal(self):
-        self.assertNotAlmostEqual(round(1.8), -1)
+def test_dummy_test__different_input__returns_not_equal():
+    assert round(1.8) != -1
